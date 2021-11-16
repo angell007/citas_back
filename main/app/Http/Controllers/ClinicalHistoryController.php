a12b8a7c791907c57053147087539a3c16f8b178
DaniloCorvus <70285261+DaniloCorvus@users.noreply.github.com>
2021-11-16 09:18:01 -0500

    changes on local 16-11-2021

--- main/app/Http/Controllers/ClinicalHistoryController.php
+++ main/app/Http/Controllers/ClinicalHistoryController.php
@@ -10,24 +10,26 @@ use Illuminate\Support\Facades\Http;
 
 class ClinicalHistoryController extends Controller
 {
-    //
-    use ApiResponser;
+       //
+       use ApiResponser;
 
-    public function index(Request $req)
-    {
-           $identifier = request()->get('identifier');
-        
-           $response = Http::get("https://lab.globho.com/api/integration/patient/$identifier/records?api_key=794340c3-4956-4763-b8ab-145da3510100&pagingnumber=0");
+       public function index(Request $req)
+       {
+              $identifier = request()->get('identifier');
+              $response = Http::withOptions([
+                     'verify' => false,
+              ])->get("https://lab.globho.com/api/integration/patient/$identifier/records?api_key=794340c3-4956-4763-b8ab-145da3510100&pagingnumber=0");
 
-           return $this->success( $response->json());
-    }
-    
-    public function show(Request $req)
-    {
-           $id = request()->get('id');
-        
-           $response = Http::get("https://lab.globho.com/api/integration/records/$id?api_key=794340c3-4956-4763-b8ab-145da3510100");
+              return $this->success($response->json());
+       }
 
-           return $this->success( $response->json());
-    }
+       public function show(Request $req)
+       {
+              $id = request()->get('id');
+              $response = Http::withOptions([
+                     'verify' => false,
+              ])->get("https://lab.globho.com/api/integration/records/$id?api_key=794340c3-4956-4763-b8ab-145da3510100");
+
+              return $this->success($response->json());
+       }
 }
